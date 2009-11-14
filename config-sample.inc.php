161d4b70d1ea3667ed645473971c5d6e287c9c58
Ryan Parman <ryan@ryanparman.com>
2009-11-14 10:23:29 -0800

    Initial import.

---
+++ config-sample.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * File: Configuration
+ * 	Stores your Flickr API information. Add your account information, then rename this file to 'config.inc.php'.
+ *
+ * Version:
+ * 	2009.11.14
+ *
+ * Copyright:
+ * 	2009 Ryan Parman
+ *
+ * License:
+ * 	Simplified BSD License - http://opensource.org/licenses/bsd-license.php
+ */
+
+
+/**
+ * Constant: FLICKR_KEY
+ * 	Flickr API Key. <http://www.flickr.com/services/api/keys/>
+ */
+define('FLICKR_KEY', '');
+
+/**
+ * Constant: FLICKR_SECRET_KEY
+ * 	Flickr API Secret Key. <http://www.flickr.com/services/api/keys/>
+ */
+define('FLICKR_SECRET_KEY', '');
